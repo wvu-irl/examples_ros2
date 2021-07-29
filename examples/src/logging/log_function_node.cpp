4b6bfa505066dde334c6f93641d0d594194d2c85
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2021-07-28 20:10:02 -0400

    Minor update

--- examples/src/logging/log_function_node.cpp
+++ examples/src/logging/log_function_node.cpp
@@ -11,7 +11,7 @@
 #include "rclcpp/rclcpp.hpp"
 
 
-// TODO: Try lambdas and make work for nonmember functions of struct/class
+// TODO: Try to make work for non-static member functions of struct/class
 
 int a_true_function()
 {
